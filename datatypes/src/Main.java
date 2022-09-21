9e0fa63cac4b91a9fe8d1dfdc5bb8b7b7f0ecb41
buraktskn <114096004+buraktskn@users.noreply.github.com>
2022-09-21 19:40:33 +0300

    Java(1.Hafta)

---
+++ datatypes/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double sayi = 12.5;
+         sayi =1.3;
+         int sayii =12;
+         sayii =11;
+         char karakter = 'A';
+         //ascii a A he birinin sayısal karşılığı vardır
+         String sehir ="ANKARA";
+         //boolean true false
+         boolean dogruMu =false;
+
+         if(dogruMu==true){
+
+         }
+    }
+}
---
+++ variables/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+//reusability
+    public static void main(String[] args) {
+        int sayi = 12;
+        String mesaj = "Öğrenci Sayısı :" ;
+        System.out.println(mesaj + sayi);
+        System.out.println(mesaj + sayi);
+        System.out.println(mesaj + sayi);
+        System.out.println(mesaj + sayi);
+        System.out.println(mesaj + sayi);
+
+
+
+
+    }
+}
